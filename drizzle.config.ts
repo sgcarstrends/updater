666f11aaf3e22cc25792617522870872e52439cb
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-12-15 03:19:19 +0800

    Unable to use SST Secret for drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "drizzle-kit";
-import { Resource } from "sst";
 
 export default defineConfig({
 	schema: "./src/schema.ts",
 	out: "./migrations",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: Resource.DATABASE_URL.value,
+		url: process.env.DATABASE_URL,
 	},
 });
