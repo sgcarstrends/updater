4b6b804e114a7f24d0732d4f636bd12ecf370bfb
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-02-19 00:13:31 +0800

    Update process.env to use secrets from SST

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,8 +1,9 @@
 import { Redis } from "@upstash/redis";
+import { Resource } from "sst";
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: Resource.UPSTASH_REDIS_REST_URL.value,
+  token: Resource.UPSTASH_REDIS_REST_TOKEN.value,
 });
 
 export default redis;
--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { Resource } from "sst";
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(Resource.DATABASE_URL.value);
 export const db = drizzle({ client: sql });
